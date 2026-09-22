c05ac8ca863706d5cf3e75739c002c71e7d98873
Jatinkanara21 <jatinkanara2006@gmail.com>
2026-09-22 10:08:50 +0530

    Build Flutter foundation: mobile/lib/main.dart

---
+++ mobile/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() => runApp(const AgriSmartApp());
+
+class AgriSmartApp extends StatelessWidget {
+  const AgriSmartApp({super.key});
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'AgriSmart',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7D32)), useMaterial3: true),
+    home: const LoginScreen(),
+  );
+}
